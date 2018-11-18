d4ab10e9a8e53a520d168b2392428410f0f6e3f2
Edgar Daniel Rojas Vazquez <edgardanielprog@gmail.com>
2018-11-17 20:02:02 -0600

    Added usefull comments

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,24 +1,28 @@
 # Program By Edgar Daniel Rojas Vazquez
 def sortFunc(lista):
-    newList = [];
+    newList = []; # Create list where we will store values and a helper list
     temp = [];
-    if len(lista) == 1:
+    if len(lista) <= 1: # If list is 1 return value or return empty list if empty
         return lista
     else:
-        if lista[0]>lista[1]:
+        if lista[0]>lista[1]: # If current number is greater than next
+            # we append the smaller number first and then we add
+            # the next recursive call which will return a bubbled up list
             newList.append(lista[1])
             temp.extend(lista[2:])
             temp.insert(0,lista[0])
             newList.extend(sortFunc(temp))
-        else:
+        else: # If number is less than, we append it first and then 
+            # append the next recursive call to the function which
+            # will return a bubbled up list
             newList.append(lista[0])
-            temp.extend(lista[1:])
-            newList.extend(sortFunc(temp))
+            newList.extend(sortFunc(lista[1:]))
     return newList
 
-listToSort = [5,4,3,2,1,'c']
-listToSort = [int(s) if str(s).isdigit() else 0 for s in listToSort]
-for i in range(len(listToSort)):
+listToSort = [5,4,3,2,1,'c',1,2,3,4,5] # Get our list values
+listToSort = [int(s) if str(s).isdigit() else 0 for s in listToSort] # Remove any symbols using a comprehension list
+for i in range(len(listToSort)): # Iterate the length of the list to ensure
+    # all of the elements are sorted
     listToSort = sortFunc(listToSort)
 print(listToSort);
 
--- quicksort.py
+++ quicksort.py
@@ -1,17 +1,20 @@
+#Program by Edgar Daniel Rojas Vazquez
 def sortFunc(lista):
-    newList = [];
-    if len(lista) <= 1:
+    newList = []; # Create Empty List to store sorted list
+    if len(lista) <= 1: # If length is 1 or less, return the element or empty list
         return lista
-    else:
+    else: # Else, do the quicksort algorithm, setting a pivot and sorting numbers less than
+        # less than the pivot to the left and more to the right
+        # all of this is done with a comprehension list for each case.
         newList.extend(sortFunc([s for s in lista[1:] if s<lista[0]]))
         newList.append(lista[0])
         newList.extend(sortFunc([s for s in lista[1:] if s>=lista[0]]))
-    return newList
+    return newList # Return the new list
 
-listToSort = [5,4,3,2,1,'c',1,2,3,4,5]
-listToSort = [int(s) if str(s).isdigit() else 0 for s in listToSort]
-listToSort = sortFunc(listToSort)
-print(listToSort);
+listToSort = [5,4,3,2,1,'c',1,2,3,4,5] # Get our list values
+listToSort = [int(s) if str(s).isdigit() else 0 for s in listToSort] # Remove any symbols using a comprehension list
+listToSort = sortFunc(listToSort) # Call the function
+print(listToSort); # Print results
 
 # The easy way to do it
 def sortFuncNormalPerson(lista):
